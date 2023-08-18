56c66577dcd64a906e80606815ace4dc7f43f3e8
puddletowntom <ronandriscoll@outlook.com>
2023-08-18 17:56:25 +0100

    Added various Bitcoin stats from blockchain api

--- main/httpGetClient.c
+++ main/httpGetClient.c
@@ -28,6 +28,32 @@ static const char *TAG = "BTC_PRICE";
 char dispTxt[20];
 bool toggle = false;
 
+typedef struct{
+    char *stats;
+    char *bcperblock;
+    char *avgtxnumber;
+    char *marketcap;
+    char *hrbtcsent;
+    char *transactionCount;
+    char *btcTicker;
+    char statsKeys[8][50];
+}BlockchainAPI;
+
+typedef struct{
+    char *api_url;
+    bool statsFlag;
+}TaskParams;
+
+BlockchainAPI blockchainAPI = {
+    .stats = "https://api.blockchain.info/stats",
+    .bcperblock = "https://blockchain.info/q/bcperblock",
+    .avgtxnumber = "https://blockchain.info/q/avgtxnumber",
+    .marketcap = "https://blockchain.info/q/marketcap",
+    .hrbtcsent = "https://blockchain.info/q/24hrbtcsent",
+    .transactionCount = "https://blockchain.info/q/24hrtransactioncount",
+    .btcTicker = "https://api.blockchain.com/v3/exchange/tickers/BTC-USD",
+    {"market_price_usd", "hash_rate", "total_fees_btc", "totalbc", "trade_volume_usd", "n_blocks_total", "minutes_between_blocks", "blocks_size"},
+};
 
 static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
@@ -74,50 +100,83 @@ void wifi_connection()
     esp_wifi_connect();
 }
 
-double extractBTCPrice(char *jsonMsg){
-    // cJSON *root = cJSON_Parse(jsonMsg);
-    // if (root == NULL) {ESP_LOGI("..", "failed to get json");}
-    // cJSON *usdObject = GET_JSON_ITEM(root, "data");
-    // if (usdObject == NULL) {ESP_LOGI("..", "failed to get data");}
-    // cJSON *object1 = GET_JSON_ITEM(usdObject, "1");
-    // if (object1 == NULL) {ESP_LOGI("..", "failed to get 1");}
-    // cJSON *quoteObject = GET_JSON_ITEM(object1, "quote");
-    // if (quoteObject == NULL) {ESP_LOGI("..", "failed to get quote");}
-    // cJSON *usdQuoteObject = GET_JSON_ITEM(quoteObject, "USD");
-    // if (usdQuoteObject == NULL) {ESP_LOGI("..", "failed to get usd");}
-    // double value = 0.0;
-    // cJSON *item = GET_JSON_ITEM(usdQuoteObject, "price"); 
-    // if (item != NULL && cJSON_IsNumber(item)) { 
-    //     value = item->valuedouble; 
-    // } else { 
-    //     /* Handle error */ 
-    // } 
-
+double extractJsonVal(char *jsonMsg, char *jsonKey){
     cJSON *root = cJSON_Parse(jsonMsg);
     double value = 0.0;
     if (root == NULL) {
         printf("Error parsing JSON: %s\n", cJSON_GetErrorPtr());
     }
-    cJSON *last_trade_price = cJSON_GetObjectItemCaseSensitive(root, "last_trade_price");
-    if (cJSON_IsNumber(last_trade_price)) {
-        value = last_trade_price->valuedouble;
-        printf("The value of last_trade_price is %f\n", value);
+    cJSON *jsonValue = cJSON_GetObjectItemCaseSensitive(root, jsonKey);
+    if (cJSON_IsNumber(jsonValue)) {
+        value = jsonValue->valuedouble;
+        printf("The value is %f\n", value);
     } else {
-        printf("Error getting last_trade_price\n");
+        printf("Error getting message\n");
     }
     cJSON_Delete(root);
     return value;
 }
 
-esp_err_t client_event_get_handler(esp_http_client_event_handle_t evt)
+#define MAX_RESPONSE_SIZE 4096
+
+// Buffer to accumulate received data
+char responseBuffer[MAX_RESPONSE_SIZE];
+size_t responseLength = 0;
+
+esp_err_t clientStatsHandler(esp_http_client_event_handle_t evt)
+{
+    switch (evt->event_id) {
+        case HTTP_EVENT_ON_DATA:
+            // Copy received data to the buffer
+            if (responseLength + evt->data_len <= MAX_RESPONSE_SIZE) {
+                memcpy(responseBuffer + responseLength, evt->data, evt->data_len);
+                responseLength += evt->data_len;
+            } else {
+                printf("Received data exceeds buffer size\n");
+            }
+            break;
+
+        // Handle other HTTP events here
+
+        case HTTP_EVENT_ON_FINISH:
+            // Process the complete accumulated data
+            responseBuffer[responseLength] = '\0'; // Null-terminate the data
+            char *current_message = strtok(responseBuffer, "}");
+            strcat(current_message, "}");
+            printf("Received data: %s\n", current_message);
+            double price = extractJsonVal(current_message, blockchainAPI.statsKeys[0]);
+            ESP_LOGI("Price", "\n%f", price);
+            double hashrate = extractJsonVal(current_message, blockchainAPI.statsKeys[1]);
+            ESP_LOGI("Hashrate", "\n%f", hashrate);
+            double fees = extractJsonVal(current_message, blockchainAPI.statsKeys[2]);
+            ESP_LOGI("Fees", "\n%f", fees);
+            double supply = extractJsonVal(current_message, blockchainAPI.statsKeys[3]);
+            ESP_LOGI("Supply", "\n%f", supply);
+            double volume = extractJsonVal(current_message, blockchainAPI.statsKeys[4]);
+            ESP_LOGI("Volume", "\n%f", volume);
+            double blockCount = extractJsonVal(current_message, blockchainAPI.statsKeys[5]);
+            ESP_LOGI("BlockCount", "\n%f", blockCount);
+            double blockInterval = extractJsonVal(current_message, blockchainAPI.statsKeys[6]);
+            ESP_LOGI("BlockInterval", "\n%f", blockInterval);
+            double blockSize = extractJsonVal(current_message, blockchainAPI.statsKeys[7]);
+            ESP_LOGI("BlockSize", "\n%f", blockSize);
+            // Process the JSON data or perform other operations here
+            break;
+
+        default:
+            break;
+    }
+    return ESP_OK;
+}
+
+esp_err_t clientSingleHandler(esp_http_client_event_handle_t evt)
 {
     switch (evt->event_id)
     {
     case HTTP_EVENT_ON_DATA:
         printf("HTTP_EVENT_ON_DATA: %.*s\n", evt->data_len, (char *)evt->data);
-        double btcVal = extractBTCPrice((char *)evt->data);
         memset(dispTxt, 0, sizeof(dispTxt));
-        sprintf(dispTxt, "$%.0f", btcVal);
+        sprintf(dispTxt, "%s", (char *)evt->data);
         break;
 
     default:
@@ -126,24 +185,31 @@ esp_err_t client_event_get_handler(esp_http_client_event_handle_t evt)
     return ESP_OK;
 }
 
+
 // http_header_t headers[] = {
 //     { "Accepts", "application/json" },
 //     { "X-CMC_PRO_API_KEY", "91c1467c-1de3-4cf1-874f-bd82e4685fa1" },
 //     { NULL, NULL }
 // };
 
-void btc_price_task(void *pvParameters) {
+void btc_api_task(void *pvParameters) {
+
+    TaskParams *params = (TaskParams*)pvParameters;
+
     esp_http_client_config_t config = {
-        .url = "https://api.blockchain.com/v3/exchange/tickers/BTC-USD", //"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC&convert=USD" 
+        .url = params->api_url, //"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC&convert=USD" 
         .method = HTTP_METHOD_GET,
         .cert_pem = NULL,
-        .event_handler = client_event_get_handler,
-        //.http_headers = headers,
-        // .headers = {
-        //     .Accepts = "application/json",
-        //     .X_CMC_PRO_API_KEY = "91c1467c-1de3-4cf1-874f-bd82e4685fa1",
-        //     },
+        //.event_handler = clientStatsHandler,
+        //.user_data = blockchainAPI.statsKeys[0]
     };
+
+    if(params->statsFlag){
+        config.event_handler = clientStatsHandler;
+    }else{
+        config.event_handler = clientSingleHandler;
+    }
+    
     esp_http_client_handle_t client = esp_http_client_init(&config);
     
     while (1) {
@@ -175,6 +241,23 @@ void app_main() {
     wifi_connection();
     screen_init();
     ui_init();
-    xTaskCreate(&btc_price_task, "btc_price_task", 4096, NULL, 5, NULL);
-    xTaskCreatePinnedToCore(lvgl_task, "LCD", 8 * 1024, NULL, 5, NULL, 1);
+    
+    TaskParams *params = (TaskParams *)malloc(sizeof(TaskParams));
+    if (params == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    params->api_url = blockchainAPI.stats;
+    params->statsFlag = true;
+
+    // TaskParams *params = (TaskParams *)malloc(sizeof(TaskParams));
+    // if (params == NULL) {
+    //     printf("Memory allocation failed.\n");
+    //     return;
+    // }
+    // params->api_url = blockchainAPI.bcperblock;
+    // params->statsFlag = false;
+
+    xTaskCreate(&btc_api_task, "btc_stats_task", 2*4096, params, 5, NULL);
+   // xTaskCreatePinnedToCore(lvgl_task, "LCD", 8 * 1024, NULL, 5, NULL, 1);
 }
