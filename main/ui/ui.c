e89ec04b5b2a7afa4265fbbe83ecf1bfdd72674c
puddletowntom <ronandriscoll@outlook.com>
2023-09-21 23:20:30 +0100

    Added Wi-Fi connection indicator and spindle for requests

--- main/ui/ui.c
+++ main/ui/ui.c
@@ -11,9 +11,10 @@
 // SCREEN: ui_Screen1
 void ui_Screen1_screen_init(void);
 lv_obj_t * ui_Screen1;
-lv_obj_t * ui_Image8;
+lv_obj_t * ui_Image3;
+//lv_obj_t * ui_Image8;
 lv_obj_t * ui_Label1;
-lv_obj_t * ui_Image11;
+//lv_obj_t * ui_Image11;
 
 // SCREEN: ui_Screen2
 void ui_Screen2_screen_init(void);
@@ -34,6 +35,8 @@ lv_obj_t * ui_Label14;
 lv_obj_t * ui_Label15;
 lv_obj_t * ui_Label16;
 lv_obj_t * ui_Label17;
+lv_obj_t * ui_S2_Label1;
+lv_obj_t * ui_S2_Label2;
 
 lv_obj_t * ui_Panel1;
 lv_obj_t * ui_Panel2;
@@ -43,11 +46,13 @@ lv_obj_t * ui_Panel5;
 lv_obj_t * ui_Panel6;
 lv_obj_t * ui_Panel7;
 lv_obj_t * ui_Panel8;
+lv_obj_t * ui_Panel10;
+lv_obj_t * ui_Spinner1;
 
-lv_obj_t * ui_Image2;
+//lv_obj_t * ui_Image2;
 lv_obj_t * ui____initial_actions0;
-const lv_img_dsc_t * ui_imgset_btc_logo[1] = {&ui_img_btc_logo1_png};
-const lv_img_dsc_t * ui_imgset_wifi_logo[2] = {&ui_img_wifi_logo2_png, &ui_img_wifi_logo3_png};
+//const lv_img_dsc_t * ui_imgset_btc_logo[1] = {&ui_img_btc_logo1_png};
+//const lv_img_dsc_t * ui_imgset_wifi_logo[2] = {&ui_img_wifi_logo2_png, &ui_img_wifi_logo3_png};
 
 ///////////////////// TEST LVGL SETTINGS ////////////////////
 #if LV_COLOR_DEPTH != 16
@@ -70,7 +75,8 @@ void ui_init(void)
                                                true, LV_FONT_DEFAULT);
     lv_disp_set_theme(dispp, theme);
     ui_Screen1_screen_init();
-    //ui_Screen2_screen_init();
+    ui_Screen2_screen_init();
     ui____initial_actions0 = lv_obj_create(NULL);
     lv_disp_load_scr(ui_Screen1);
+    
 }
